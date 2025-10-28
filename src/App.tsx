160657409c2b0d7376ee3fc8792969d5455987d5
Maya Thompson <majsant96@hotmail.com>
2025-10-28 21:00:15 +0100

    add routes and nav links to App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-function App() {
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import CheckoutSuccess from "./pages/CheckoutSuccess";
+
+const App = () => {
   return (
     <div className="App">
-      <h1>Online Shop</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/cart">Cart</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout-success" element={<CheckoutSuccess />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
