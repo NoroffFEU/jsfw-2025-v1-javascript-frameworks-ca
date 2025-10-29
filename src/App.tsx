502198750b2570333683593f6216d29202eea74b
Maya Thompson <majsant96@hotmail.com>
2025-10-29 16:21:25 +0100

    add header file with unstyled elements and update App file with header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
 import Contact from "./pages/Contact";
@@ -7,20 +8,17 @@ import CheckoutSuccess from "./pages/CheckoutSuccess";
 
 const App = () => {
   return (
-    <div className="App">
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/cart">Cart</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<ProductDetail />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout-success" element={<CheckoutSuccess />} />
-      </Routes>
+    <div className="d-flex flex-column min-vh-100">
+      <Header />
+      <main className="flex-grow-1 container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout-success" element={<CheckoutSuccess />} />
+        </Routes>
+      </main>
     </div>
   );
 };
---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <Navbar>
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          Online Shop
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav>
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/contact">
+              Contact
+            </Nav.Link>
+            <Nav.Link as={Link} to="/cart">
+              Cart
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
